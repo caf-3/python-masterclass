7257c8b5139367ac286a06dd9afe2956c4edcff1
caf-3 <tomas.caetano@ubi.co.mz>
2022-01-18 22:13:07 +0200

    string methods initialized

---
+++ strings/main.py
@@ -0,0 +1,13 @@
+name = "Youtube Code Channel"
+city = 'maputo'
+is_digit = '123'
+print(len(name))
+print(name.find('Y'))
+print(city.capitalize())
+print(city.upper())
+print(name.lower())
+print(is_digit.isdigit())
+print(city.isalpha())
+print(name.count("o"))
+print(city.replace("a", "o "))
+print("I am from: "+str(city*4))
