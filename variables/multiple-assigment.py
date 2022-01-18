2f3ec748748826b3028093cd5517d0029123d5d5
caf-3 <tomas.caetano@ubi.co.mz>
2022-01-18 21:55:23 +0200

    python multiple assignment

---
+++ variables/multiple-assigment.py
@@ -0,0 +1,9 @@
+name, age, is_human = "Tomas", 21, True
+print("Name: "+name)
+print("Age: "+str(age))
+print("Is human: "+str(is_human))
+
+#Ages
+Tomas = Fredricksson = 30
+print("Tomas "+str(Tomas))
+print("Fredricksson "+str(Fredricksson))
