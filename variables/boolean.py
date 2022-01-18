ffba0b802281d60bb465c9816baa8c1eb18f924a
caf-3 <tomas.caetano@ubi.co.mz>
2022-01-18 21:36:32 +0200

    boolean datatype added

---
+++ variables/boolean.py
@@ -0,0 +1,5 @@
+human = False
+robot = True
+
+print("Q: Are you human ?\nA: "+str(human))
+print("Q: Are you robot ?\nA: "+str(robot))
--- variables/main.py
+++ variables/main.py
@@ -2,7 +2,7 @@ name = 'Tomas'
 print(name)
 #Check the variable type
 print(type(name))
-#Concatenante two variables
+#Cocatenante two variables
 first_name = 'Bro'
 last_name = 'Caetano'
 full_name = first_name + ' ' + last_name
