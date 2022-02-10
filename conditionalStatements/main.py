7bdfcf5a5e434a129cdfba36e5794bfd6f9a7c80
caf-3 <tomas.caetano@ubi.co.mz>
2022-02-10 22:03:02 +0200

    conditional statements

---
+++ conditionalStatements/main.py
@@ -0,0 +1,8 @@
+age = int(input("What is your age: "))
+
+if age == 100:
+    print('You have a century here. Congratulations!')
+elif age >= 18:
+    print('You are an adult!')
+else:
+    print('Provide your real age')
