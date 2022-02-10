4e2e98978d39127674ec170bedee38840db4de93
caf-3 <tomas.caetano@ubi.co.mz>
2022-02-10 21:29:16 +0200

    string slicing

---
+++ strings/slicing.py
@@ -0,0 +1,11 @@
+name = "Tomas Caetano"
+first_name = name[:5]
+last_name = name[6:]
+reversed_name = name[::-1]
+
+website1 = 'http://google.com'
+website2 = 'http://wikipedia.com'
+sliced = slice(7, -4)
+sliced_website = website2[sliced]
+
+print(sliced_website)
