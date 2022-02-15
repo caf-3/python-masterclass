9b9f4a1888b6174742116de9aa01f988e9932a7d
caf-3 <tomas.caetano@ubi.co.mz>
2022-02-15 04:24:03 +0200

    for loop added

---
+++ loops/for.py
@@ -0,0 +1,18 @@
+import time
+
+for i in range(10):
+    print(i+1)
+
+for num in range(50, 101):
+    print(num)
+
+print("even numbers")
+
+for even in range(0, 50, 2):
+    print(even)
+
+for seconds in range(10, -1, -1):
+    print(seconds)
+    time.sleep(1)
+
+print("Happy new year!")
