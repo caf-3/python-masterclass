c377c1109fdbaa10c78200d81dd7ccd6ccf50e5b
caf-3 <tomas.caetano@ubi.co.mz>
2022-02-17 03:32:02 +0200

    lists added

---
+++ lists/main.py
@@ -0,0 +1,13 @@
+# list = is used to store multiple items in a single variable (similar to Array)
+
+school_subjects = ["Matematica", "Português", "Quimica", "Filosofia"]
+
+# school_subjects.append("Geografia")
+# school_subjects.remove("Quimica")
+# school_subjects.pop()
+# school_subjects.insert(1, "Biologia")
+# school_subjects.sort()
+# school_subjects.clear()
+
+for subject in school_subjects:
+    print(subject)
