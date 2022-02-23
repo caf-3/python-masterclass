0b640ad48aa6848072286a5b1785b8149fc1e751
caf-3 <tomas.caetano@ubi.co.mz>
2022-02-23 22:16:22 +0200

    nested function calls

---
+++ functions/nestedFnCalls.py
@@ -0,0 +1,9 @@
+# num = input("Digite um número inteiro positivo: ")
+# num = float(num)
+# num = abs(num)
+# num = round(num)
+
+# print(f'You entered this num: {num}')
+
+
+print(round(abs(float(input("Digite um número inteiro positivo: ")))))
